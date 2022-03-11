07646c096fa3c91d27900b3985411b7751e205d4
Ignacio Coluccio <ignacio.coluccio@gmail.com>
2022-03-11 18:18:53 -0300

    Code review

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,24 @@ class BooksController < ApplicationController
     render_paginated json: Book.all, each_serializer: BookSerializer
   end
 
+  def listar_libros_de_gente
+    libros = Book.where.not(rent: nil)
+    libros.select{|libro|
+      libro.image.present?
+    }
+    Book.new.remove_all_books_without_author
+    render_paginated json: libros, each_serializer: BookSerializer
+  rescue Exception => e
+    Book.new.remove_all_books_without_author
+    render_paginated json: Book.all, each_serializer: BookSerializer
+  end
+
+  def clean_books
+    libros = listar_libros_de_gente
+    libros.delete_all!
+    render_paginated json: libros.first.show_as_string, each_serializer: BookSerializer
+  end
+
   def book_information
     render json: OpenLibraryBooksService.new.retrieve_book_information_by_isbn(isbn_param)
   rescue RuntimeError => e
